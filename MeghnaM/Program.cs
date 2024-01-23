bbd16a67eda15897b1d834d605319626280be556
Judith Buck <judith18buckden@hotmail.com>
2024-01-23 20:22:08 +0000

    added student pesudocode

---
+++ MeghnaM/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeghnaM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullname = "Jim Daley";
+            int length = fullname.Length;
+            int space = fullname.IndexOf(' ');
+            string initial = fullname.Substring(0,1);
+            //string lastname = fullname.Substring(space, length - (space));
+            string lastname = fullname.Substring(space + 1,length-(space+1));
+            Console.WriteLine("// fullname");
+            Console.WriteLine(fullname); 
+            Console.WriteLine("// length");
+            Console.WriteLine(length);
+            Console.WriteLine("// space");
+            Console.WriteLine(space);
+            Console.WriteLine("// initial");
+            Console.WriteLine(initial);
+            Console.WriteLine("// lastname");
+            Console.WriteLine(lastname);
+            Console.WriteLine("// initial + lastname");
+            //Console.WriteLine(initial + lastname);
+            Console.WriteLine(initial + ' '+lastname);
+        }
+    }
+}
---
+++ SidM/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SidM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jim Daley";            
+            Console.WriteLine("// name");
+            Console.WriteLine(name);
+            Console.WriteLine("// length");
+            int length = name.Length;
+            Console.WriteLine(length);
+            Console.WriteLine("// secondnamestart");
+            int secondnamestart = name.IndexOf(' ') + 1;
+            Console.WriteLine(secondnamestart);
+            Console.WriteLine("// initial1");
+            string initial1 = name.Substring(0, 1);
+            Console.WriteLine(initial1);
+            Console.WriteLine("// lastname");
+            string lastname = name.Substring(secondnamestart, length-secondnamestart); 
+            Console.WriteLine(lastname);
+            Console.WriteLine("// initial1 + ' '+ lastname");
+            Console.WriteLine(initial1 + ' '+ lastname);
+        }
+    }
+}
